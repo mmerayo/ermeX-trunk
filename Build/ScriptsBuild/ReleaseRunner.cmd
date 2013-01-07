c6706fb4bb5a95e54cc16aaeba07b3c50483ae15
mmerayo <miguelmerayo@hotmail.com>
2013-01-07 14:14:28 +0100

    fixed acceptance tester

--- Build/ScriptsBuild/ReleaseRunner.cmd
+++ Build/ScriptsBuild/ReleaseRunner.cmd
@@ -12,12 +12,13 @@ set configuration=%CI_Configuration%
 
 REM COMMON TO ALL BUILDS
 set artifactsFolder=%CD%\..\BuildArtifacts
-set outputBinFolder=%artifactsfolder%\Binaries
-set outputReportsFolder=%artifactsfolder%\Reports
+set outputBinFolder=%artifactsfolder%\Binaries\%frameworkVersion%\%buildType%\%configuration%
+set mergedFolder=%artifactsfolder%\Merged\v%frameworkVersion%\%buildType%
+set outputReportsFolder=%artifactsfolder%\Reports\%frameworkVersion%\%buildType%
 set packagesFolder=%CD%\..\Packages
 set nunitExePath=%packagesFolder%\nUnit\NUnit-2.5.6.10205\bin
 set taskId=%1
-set msBuildProperties=Configuration=%CI_Configuration%;OutputPath="%outputBinFolder%"\;Platform=%buildType%;Optimize=true;BuildReportsFolder="%outputReportsFolder%";TaskIdName=%taskId%;nUnitPath="%nunitExePath%";nUnitFramework=%nunitFramework%
+set msBuildProperties=Configuration=%configuration%;OutputPath="%outputBinFolder%"\;Platform=%buildType%;Optimize=true;BuildReportsFolder="%outputReportsFolder%";TaskIdName=%taskId%;nUnitPath="%nunitExePath%";nUnitFramework=%nunitFramework%;MergedFolder="%mergedFolder%"
 
 
 
